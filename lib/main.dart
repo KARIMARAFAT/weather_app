5e90afc5daf3823fab1df5187af5144847969d15
KARIMARAFAT <karimarafat2112@gmail.com>
2026-09-15 12:48:40 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/features/screen/view/home_view.dart';
+
+void main() {
+  runApp(const WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeView());
+  }
+}
